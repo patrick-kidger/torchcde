4bac30cfa2a3c36355bdfe2881bb1fba2cbd358e
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2020-09-08 18:54:15 +0100

    updated torch requirements

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ classifiers = ["Development Status :: 4 - Beta",
 
 python_requires = "~=3.6"
 
-install_requires = ['torch>=1.0.0', 'torchdiffeq>=0.1.0']
+install_requires = ['torch>=1.6.0', 'torchdiffeq>=0.1.0']
 
 setuptools.setup(name=name,
                  version=version,
