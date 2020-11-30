9194689c660169f1cea5780fe5c7afca6fb51676
James Morrill <james.morrill.6@gmail.com>
2020-11-30 15:19:53 +0000

    Updated a test for correct behaviour of torch.cummax()

--- test/test_misc.py
+++ test/test_misc.py
@@ -29,18 +29,29 @@ def test_tridiagonal_solve():
 
 
 def test_forward_fill():
-    for N, L, C in [(1, 2, 3), (2, 2, 2), (3, 2, 1)]:
-        x = torch.randn(N, L, C)
-        # Drop mask
-        tensor_num = x.numel()
-        mask = torch.randperm(tensor_num)[:int(0.3 * tensor_num)]
-        x.view(-1)[mask] = float('nan')
-        x_ffilled = x.clone().float()
-        for i in range(0, x.size(0)):
-            for j in range(x.size(1)):
-                for k in range(x.size(2)):
-                    non_nan = x_ffilled[i, :j + 1, k][~torch.isnan(x[i, :j + 1, k])]
-                    input_val = non_nan[-1].item() if len(non_nan) > 0 else float('nan')
-                    x_ffilled[i, j, k] = input_val
-        x_ffilled_actual = torchcde.misc.forward_fill(x)
-        assert x_ffilled.allclose(x_ffilled_actual, equal_nan=True)
+    devices = ['cpu']
+    if torch.cuda.is_available():
+        devices.append('cuda')
+
+    for device in devices:
+        # Check indices returns
+        indices_check = torch.tensor([False, False, True, True]).to(device).cummax(0).indices
+        desired_indices = torch.tensor([0, 1, 2, 3]).to(device)
+        assert torch.equal(indices_check, desired_indices)
+
+        # Check ffill
+        for N, L, C in [(1, 5, 3), (2, 2, 2), (3, 2, 1)]:
+            x = torch.randn(N, L, C).to(device)
+            # Drop mask
+            tensor_num = x.numel()
+            mask = torch.randperm(tensor_num)[:int(0.3 * tensor_num)].to(device)
+            x.view(-1)[mask] = float('nan')
+            x_ffilled = x.clone().float()
+            for i in range(0, x.size(0)):
+                for j in range(x.size(1)):
+                    for k in range(x.size(2)):
+                        non_nan = x_ffilled[i, :j + 1, k][~torch.isnan(x[i, :j + 1, k])]
+                        input_val = non_nan[-1].item() if len(non_nan) > 0 else float('nan')
+                        x_ffilled[i, j, k] = input_val
+            x_ffilled_actual = torchcde.misc.forward_fill(x)
+            assert x_ffilled.allclose(x_ffilled_actual, equal_nan=True)
