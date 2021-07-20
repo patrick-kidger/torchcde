3ac1f701ed804b60e7148c70589fc638b6bb48ae
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2021-07-20 23:00:17 +0100

    fix test

--- test/test_example.py
+++ test/test_example.py
@@ -4,18 +4,18 @@ import sys
 _here = pathlib.Path(__file__).resolve().parent
 sys.path.append(str(_here / '../example'))
 
-import example
-import logsignature_example
 import irregular_data
-
-
-def test_example():
-    example.main(num_epochs=3)
-
-
-def test_logsignature_example():
-    logsignature_example.main(num_epochs=1)
+import logsignature_example
+import time_series_classification
 
 
 def test_irregular_data():
     irregular_data.irregular_data()
+
+
+def test_time_series_classification():
+    time_series_classification.main(num_epochs=3)
+
+
+def test_logsignature_example():
+    logsignature_example.main(num_epochs=1)
