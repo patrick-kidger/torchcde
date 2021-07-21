de71d4c07cb14fcb460018567fb6f8f98922d22b
Michael Poli <34561392+Zymrael@users.noreply.github.com>
2021-07-21 18:29:11 +0900

    Add CI workflow for PyPI publishing. Poetry config. (#30)
    * add publishing workflow and poetry config
    
    * fix typo
    
    * removed poetry, added CI script, modified publishing script
    
    * added workflows
    
    * modify name of second wflow
    
    * newlines..
    
    * fix typo in second workflow
    
    * various small changes
    
    * on release check added at start of publish action

--- setup.py
+++ setup.py
@@ -48,8 +48,7 @@ classifiers = ["Development Status :: 4 - Beta",
 
 python_requires = "~=3.6"
 
-install_requires = ['torch>=1.7.0', 'torchdiffeq>=0.2.0',
-                    'torchsde @ git+https://github.com/google-research/torchsde.git']
+install_requires = ['torch>=1.7.0', 'torchdiffeq>=0.2.0', 'torchsde>=0.2.5']
 
 setuptools.setup(name=name,
                  version=version,
